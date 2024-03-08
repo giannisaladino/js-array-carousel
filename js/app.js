3727627032090d6ec6e63830e584ea0d7454bcc0
giannisaladino <saladinogianni6@gmail.com>
2024-03-08 16:54:04 +0100

    EDIT: css

--- js/app.js
+++ js/app.js
@@ -4,4 +4,36 @@ const arrayImages = [              //array - string
     "./img/03.webp",
     "./img/04.webp",
     "./img/05.webp"
-]
+];
+
+// recupero l'elemento 'items-container'
+
+const itemsContainerDOMElement = document.querySelector('.items-container');
+
+let itemsContent = '';
+
+// inserisco all'interno degli 'items-container' le immagini
+
+    // uso un ciclo for per iterare l'array arrayImages
+
+for(let index = 0; index < arrayImages.length; index++) {
+
+    const imagePath = arrayImages[index];
+
+    const sliderItem = `
+    <div class="item">
+         <img src="${imagePath}">
+    </div>
+    `
+    // per ogni elemento dell'array creo una parte di HTML
+
+    itemsContent += sliderItem; 
+}
+
+itemsContainerDOMElement.innerHTML = itemsContent;
+
+
+const divItem = document.getElementsByClassName('item');
+
+// aggiungo la classe 'active' al primo item
+divItem[0].classList.add('active');
